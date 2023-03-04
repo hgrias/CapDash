a28bafbd6dec764840af6f7a421cac4ae1c08759
Harrison Grias <harrison.grias@gmail.com>
2023-03-03 19:36:21 -0600

    feat: add header to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,24 @@ import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
+import { Header } from "~/components/header";
+
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Texas Legislator Dashboard</title>
+        <title>Legislator Dashboard</title>
         <meta
           name="Texas Legislator Dashboard"
           content="Dashboard to view TX legislator profiles"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main>
+        <Header />
+      </main>
     </>
   );
 };
