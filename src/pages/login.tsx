b2dc153c4b6debe9256565ebdb0dbbb02c5e7fa9
Harrison Grias <harrison.grias@gmail.com>
2023-03-03 23:21:11 -0600

    chore: add TODO and clean some stuff up
    Need to implement a check to see if a user is already authenticated. If
    they are and try to visit the login page, then we need to redirect them
    to the index/dashboard page.

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,10 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { LoginForm } from "~/components/loginForm";
-import { redirect } from "next/navigation";
 
 const Login: NextPage = () => {
   const { data: sessionData } = useSession();
-
+    // TODO: Redirect to index page if user is already logged in
   return (
     <div className="auth-container my-20">
       <div className="">
