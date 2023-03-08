f3a1bc5e481803345fcb225132774fe9c20adcca
Harrison Grias <harrison.grias@gmail.com>
2023-03-08 14:18:34 -0600

    feat: Add GCP/GCS values to env

--- src/env.mjs
+++ src/env.mjs
@@ -21,6 +21,9 @@ const server = z.object({
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
+  GCP_PROJECT_ID: z.string(),
+  GCP_KEY_FILENAME: z.string(),
+  GCS_BUCKET_NAME: z.string()
 });
 
 /**
@@ -44,6 +47,9 @@ const processEnv = {
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GCP_PROJECT_ID: process.env.GCP_PROJECT_ID,
+  GCP_KEY_FILENAME: process.env.GCP_KEY_FILENAME,
+  GCS_BUCKET_NAME: process.env.GCS_BUCKET_NAME
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
