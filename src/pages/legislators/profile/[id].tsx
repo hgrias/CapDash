d5dcf7244c1243d87e4ce82f74912e1594f2edcf
Harrison Grias <harrison.grias@gmail.com>
2023-03-08 15:02:44 -0600

    feat: Initial working version of profile page
    WIP: Not adhering to some TS hints. These will be fixed when refactoring
    to context/compound components

--- src/pages/legislators/profile/[id].tsx
+++ src/pages/legislators/profile/[id].tsx
@@ -9,83 +9,20 @@ import { type NextPage } from "next";
 import { api } from "~/utils/api";
 import Link from "next/link";
 
-export default function LegislatorPage(legislator: Legislator) {
-  const router = useRouter();
-  const { legislatorId } = router.query;
-  const title = `${legislator.firstName} ${legislator.lastName} | Profile`;
-
-  // Return a loading div while the page is being rendered
-  if (router.isFallback) {
-    return <div>Loading...</div>;
-  }
+const LegislatorProfile: NextPage = () => {
+  const legislatorId = useRouter().query.id as string;
+  // TODO: Determine if this API call is called on blur only on dev?
+  const { data: legislator } = api.legislator.get.useQuery({ legislatorId });
 
   return (
     <>
       <Header />
-      <Avatar imageUri={legislator?.imageUri} />
-      <h1>Legislator Page</h1>
+      <div className="flex items-center justify-center">
+        <Avatar imageUri={legislator?.imageUri} />
+        <Bio {...legislator} />
+      </div>
     </>
   );
-}
-
-// Use this to fetch an array of legislator IDs for our dynamic routes
-export async function getStaticPaths() {
-  //   const { data: session, status } = useSession();
-  const { data: legislators } = api.legislator.getAllIds.useQuery();
-
-  // TODO: Determine what to do if there are no IDs
-  if (!legislators) {
-    return;
-  }
-
-  return {
-    paths: legislators.map((legislator) => {
-      return {
-        params: {
-          id: legislator.id,
-        },
-      };
-    }),
-    // fallback: true means that the missing pages
-    // will not 404_app, and instead can render a fallback.
-    fallback: true,
-  };
-}
-
-type Params = {
-  params: {
-    id: string;
-  };
 };
 
-// Use this to fetch data for each Legislator
-export async function getStaticProps({ params }: Params) {
-  const { data: legislator } = api.legislator.get.useQuery({
-    legislatorId: params.id,
-  });
-
-  return {
-    props: {
-      legislator: {
-        ...legislator,
-      },
-    },
-    // Incremental Static Regeneration after 60 sec
-    revalidate: 60,
-  };
-}
-
-type Legislator = {
-  id: string;
-  firstName: string;
-  lastName: string;
-  state: State;
-  party: Party;
-  chamber: Chamber;
-  district: number;
-  imageUri: string | undefined;
-  chamberWebsiteUrl: string;
-  capitolWebsiteUrl: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
+export default LegislatorProfile;
