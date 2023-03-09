64b55160d742099bed4bf79feeafc9fe6f72b7db
Harrison Grias <harrison.grias@gmail.com>
2023-03-09 12:37:18 -0600

    feat: move avatar into bio card component

--- src/components/legislator/bio.tsx
+++ src/components/legislator/bio.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useContext } from "react";
 import LegislatorContext from "./legislatorContext";
+import Avatar from "./avatar";
 
 interface BioProps {
   firstName: string;
@@ -9,7 +10,8 @@ interface BioProps {
   chamber: string;
 }
 
-const Bio: FC<BioProps> = () => {
+// TODO: Do I still need BioProps if we are getting data directly from context?
+const Bio: FC = () => {
   const { firstName, lastName, district, party, chamber } =
     useContext(LegislatorContext);
 
@@ -17,6 +19,7 @@ const Bio: FC<BioProps> = () => {
     <>
       <div className="card w-96 bg-neutral text-neutral-content">
         <div className="card-body items-center text-center">
+          <Avatar />
           <h1 className="card-title">
             {firstName} {lastName}
           </h1>
--- src/components/legislator/profileDetails.tsx
+++ src/components/legislator/profileDetails.tsx
@@ -1,12 +1,9 @@
 import React, { FC } from "react";
 import Bio from "./bio";
-import Avatar from "./avatar";
-import LegislatorContext from "./legislatorContext";
 
 const ProfileDetails: FC = () => {
   return (
     <>
-      <Avatar />
       <Bio />
     </>
   );
