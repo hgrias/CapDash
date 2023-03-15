40d2d013f9be029a0a5c75e0dc5a9580180bc627
Harrison Grias <harrison.grias@gmail.com>
2023-03-15 10:13:20 -0500

    feat: Initial version of new interaction form

--- src/components/interaction/newInteractionForm.tsx
+++ src/components/interaction/newInteractionForm.tsx
@@ -13,24 +13,39 @@ const NewInteractionForm: FC<NewInteractionFormProps> = () => {
   return (
     <>
       <div className="card bg-primary text-primary-content">
-        <div className="card-body items-center text-center">
+        <div className="card-body">
           <div className="card-title">Create New Interaction</div>
           <div className="card-content">
-            <div className="form-control w-full max-w-xs">
-              <label className="label">
-                <span className="label-text">Interaction Type</span>
-              </label>
-              <select className="select-bordered select">
-                {interactionTypeOptions}
-              </select>
-              <label className="label">
-                <span className="label-text">What is your name?</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Type here"
-                className="input-bordered input w-full max-w-xs"
-              />
+            <div className="form form-control grid grid-cols-2 gap-4">
+              <div className="col-span-2">
+                <label className="label">
+                  <span className="label-text">Content</span>
+                </label>
+                <textarea
+                  name="interactionContent"
+                  className="textarea-bordered textarea h-52 w-full"
+                  placeholder="Interaction Content"
+                />
+              </div>
+              <div className="">
+                <label className="label">
+                  <span className="label-text">Interaction Type</span>
+                </label>
+                <select
+                  name="interactionType"
+                  className="select-bordered select"
+                >
+                  {interactionTypeOptions}
+                </select>
+              </div>
+              <div className="relative">
+                <button
+                  className="btn-secondary btn absolute bottom-0 right-0"
+                  type="submit"
+                >
+                  Create
+                </button>
+              </div>
             </div>
           </div>
         </div>
