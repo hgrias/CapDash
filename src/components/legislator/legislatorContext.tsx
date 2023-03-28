761df8d765f5089fc9ce1c2d1936b2d4aaf3ca50
Harrison Grias <harrison.grias@gmail.com>
2023-03-28 15:47:33 -0500

    fix: update legislator context with correct type

--- src/components/legislator/legislatorContext.tsx
+++ src/components/legislator/legislatorContext.tsx
@@ -1,9 +1,24 @@
+import { Legislator, Party, State, Chamber } from "@prisma/client";
 import { createContext } from "react";
-import { Legislator } from "@prisma/client";
 
-const defaultLegislator = {};
+const defaultDate: Date = new Date(2023, 2, 28);
 
-export const LegislatorContext =
-  createContext<Partial<Legislator>>(defaultLegislator);
+const defaultLegislator = {
+  id: "12345",
+  firstName: "John",
+  lastName: "Doe",
+  state: State.MI,
+  party: Party.Other,
+  chamber: Chamber.Senate,
+  district: -1,
+  imageUri: "defaultValue",
+  chamberWebsiteUrl: "defaultValue",
+  capitolWebsiteUrl: "defaultValue",
+  currentSessionId: -1,
+  createdAt: defaultDate,
+  updatedAt: defaultDate,
+};
+
+export const LegislatorContext = createContext<Legislator>(defaultLegislator);
 
 export default LegislatorContext;
