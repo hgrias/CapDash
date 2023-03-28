1dbd3b6aadd73ae9c926c6f7b6d505f7c27e7860
Harrison Grias <harrison.grias@gmail.com>
2023-03-28 15:47:50 -0500

    feat: render legislator intials if no image

--- src/components/legislator/avatar.tsx
+++ src/components/legislator/avatar.tsx
@@ -1,32 +1,40 @@
-import React, { useContext } from "react";
 import LegislatorContext from "./legislatorContext";
-
-interface AvatarProps {
-  imageUri?: string;
-}
+import React, { useContext } from "react";
 
 const Avatar = () => {
-  const { imageUri } = useContext(LegislatorContext);
+  // Get necessary data from context instead of props
+  const { imageUri, firstName, lastName } = useContext(LegislatorContext);
   // TODO: Do I need to store the bucket name in an env var?
   const gcsBasePath =
     "https://storage.googleapis.com/legislator-dashboard/avatars";
   const fullImagePath = `${gcsBasePath}/${imageUri}`;
 
-  // Don't render if there is no image URI
-  // TODO: Look into using component composition / compound components for Legislator Profile
+  // If there is no image, render a placeholder with initials
   if (!imageUri) {
-    return null;
-  }
-
-  return (
-    <>
-      <div className="avatar">
-        <div className="w-38 rounded">
-          <img src={fullImagePath} />
+    const initials = `${firstName.charAt(0).toUpperCase()}${lastName
+      .charAt(0)
+      .toUpperCase()}`;
+    return (
+      <>
+        <div className="placeholder avatar">
+          <div className="w-24 rounded bg-neutral-focus text-neutral-content">
+            <span className="text-3xl">{initials}</span>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+    // Otherwise, return the image via GCS image URI
+  } else {
+    return (
+      <>
+        <div className="avatar">
+          <div className="rounded">
+            <img src={fullImagePath} />
+          </div>
+        </div>
+      </>
+    );
+  }
 };
 
 export default Avatar;
