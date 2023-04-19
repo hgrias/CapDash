fe499c8f549361d4da9c5a7dbfa551ceeca6872f
Harrison Grias <harrison.grias@gmail.com>
2023-04-19 14:52:17 -0400

    fix: query types and warnings

--- src/components/profileContext.tsx
+++ src/components/profileContext.tsx
@@ -1,13 +1,20 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { Staffer, Note, Tag } from "@prisma/client";
-import { RouterOutputs } from "~/utils/api";
-import { api } from "~/utils/api";
 import { useOrganizationContext } from "./organizationContext";
+import { api } from "~/utils/api";
+import type { UseTRPCInfiniteQueryResult } from "@trpc/react-query/shared";
+import type { interactionRouter } from "~/server/api/routers/interaction";
+import type { noteRouter } from "~/server/api/routers/note";
+import type { Staffer, Note, Tag } from "@prisma/client";
+import type { TRPCClientErrorLike } from "@trpc/client";
+import type { RouterOutputs } from "~/utils/api";
 
 // Define some types from our router/procedure outputs
+type notesListQueryType = RouterOutputs["note"]["listForLegislator"];
 type legislatorQueryType = RouterOutputs["legislator"]["byId"];
 type legislativeSessionQueryType =
   RouterOutputs["legislativeSession"]["getActiveSession"];
+type interactionsListQueryType =
+  RouterOutputs["interaction"]["getForLegislator"];
 
 // Manually define these types
 type InteractionsWithUser = {
@@ -39,8 +46,14 @@ interface ProfileContextValue {
   staffers: Staffer[];
   selectedSession: legislativeSessionQueryType;
   error?: Error;
-  notesQuery: any; // Can't find type of useInfiniteQuery output??
-  interactionsQuery: any;
+  notesQuery: UseTRPCInfiniteQueryResult<
+    notesListQueryType,
+    TRPCClientErrorLike<typeof noteRouter>
+  >;
+  interactionsQuery: UseTRPCInfiniteQueryResult<
+    interactionsListQueryType,
+    TRPCClientErrorLike<typeof interactionRouter>
+  >;
 }
 
 const ProfileContext = createContext<ProfileContextValue | null>(null);
@@ -63,7 +76,7 @@ export function ProfileProvider({
 
   const { orgActiveSession } = useOrganizationContext();
 
-  const legislatorQuery = api.legislator.byId.useQuery(
+  api.legislator.byId.useQuery(
     {
       legislatorId: legislatorId,
     },
@@ -76,27 +89,28 @@ export function ProfileProvider({
     }
   );
 
-  const notesQuery = api.note.listForLegislator.useInfiniteQuery(
-    {
-      legislatorId: legislatorId,
-      limit: 5,
-    },
-    {
-      enabled: !!legislatorId,
-      refetchOnWindowFocus: false,
-      onSuccess: (data) => {
-        if (data.pages) {
-          const newNotes = data.pages.flatMap((page) => page?.notes ?? []);
-          setNotes(newNotes);
-        }
+  const notesQuery =
+    api.note.listForLegislator.useInfiniteQuery<notesListQueryType>(
+      {
+        legislatorId: legislatorId,
+        limit: 5,
       },
-      getNextPageParam: (nextPage) => {
-        return nextPage?.nextCursor;
-      },
-    }
-  );
+      {
+        enabled: !!legislatorId,
+        refetchOnWindowFocus: false,
+        onSuccess: (data) => {
+          if (data.pages) {
+            const newNotes = data.pages.flatMap((page) => page?.notes ?? []);
+            setNotes(newNotes);
+          }
+        },
+        getNextPageParam: (nextPage) => {
+          return nextPage?.nextCursor;
+        },
+      }
+    );
 
-  const stafferQuery = api.staffer.list.useQuery(
+  api.staffer.list.useQuery(
     {
       legislatorId: legislatorId,
     },
@@ -111,27 +125,28 @@ export function ProfileProvider({
     }
   );
 
-  const interactionsQuery = api.interaction.getForLegislator.useInfiniteQuery(
-    {
-      legislatorId: legislatorId,
-      limit: 5,
-    },
-    {
-      enabled: !!legislatorId,
-      refetchOnWindowFocus: false,
-      onSuccess: (data) => {
-        if (data.pages) {
-          const newInteractions = data.pages.flatMap(
-            (page) => page?.interactions ?? []
-          );
-          setInteractions(newInteractions);
-        }
+  const interactionsQuery =
+    api.interaction.getForLegislator.useInfiniteQuery<interactionsListQueryType>(
+      {
+        legislatorId: legislatorId,
+        limit: 5,
       },
-      getNextPageParam: (nextPage) => {
-        return nextPage?.nextCursor;
-      },
-    }
-  );
+      {
+        enabled: !!legislatorId,
+        refetchOnWindowFocus: false,
+        onSuccess: (data) => {
+          if (data.pages) {
+            const newInteractions = data.pages.flatMap(
+              (page) => page?.interactions ?? []
+            );
+            setInteractions(newInteractions);
+          }
+        },
+        getNextPageParam: (nextPage) => {
+          return nextPage?.nextCursor;
+        },
+      }
+    );
 
   const value: ProfileContextValue = {
     legislator: legislator,
