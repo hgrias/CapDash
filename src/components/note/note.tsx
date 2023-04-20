76e6a95230bacd8f450d48edb6ad525802271c3e
Harrison Grias <harrison.grias@gmail.com>
2023-04-19 20:33:45 -0400

    fix: eslint errors on note component

--- src/components/note/note.tsx
+++ src/components/note/note.tsx
@@ -73,8 +73,8 @@ const Note = ({
   const deleteNote = api.note.delete.useMutation({
     onSuccess: () => {
       // Refresh the notes on profile page
-      utils.note.listForLegislator.invalidate();
-      utils.interaction.getForLegislator.invalidate();
+      void utils.note.listForLegislator.invalidate();
+      void utils.interaction.getForLegislator.invalidate();
     },
     onError: (error) => {
       console.error("Error deleting note:", error);
@@ -83,7 +83,12 @@ const Note = ({
 
   // Get current user ID from session
   const session = useSession();
-  const currentUserId = session.data!.user.id;
+
+  if (!session.data) {
+    return null;
+  }
+
+  const currentUserId = session.data.user.id;
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -100,7 +105,7 @@ const Note = ({
   return (
     <div
       className={`relative flex p-4 transition-colors hover:bg-gray-100 ${
-        isHovered && "bg-gray-100"
+        isHovered ? "bg-gray-100" : ""
       }`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
@@ -122,7 +127,7 @@ const Note = ({
           {confirmDeleteModal(handleDeleteNote)}
           <label
             htmlFor="confirmDeleteModal"
-            className="btn-outline btn-square btn-xs btn border-red-600 text-sm text-red-600 hover:border-red-600 hover:bg-red-600"
+            className="btn-outline btn-xs btn-square btn border-red-600 text-sm text-red-600 hover:border-red-600 hover:bg-red-600"
           >
             X
           </label>
