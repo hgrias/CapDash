7daf5ba163a28dbbc35af60549199651b2e4652f
Harrison Grias <harrison.grias@gmail.com>
2023-04-21 11:03:52 -0500

    feat: shadcn-ui setup

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+// make it easier to conditionally add Tailwind CSS classes with shadcn-ui
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
