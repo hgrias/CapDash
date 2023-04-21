a1d5ee3dd05d652ca099cd47d7bcd5c3903c2c77
Harrison Grias <harrison.grias@gmail.com>
2023-04-21 18:29:48 -0500

    refactor: Header logo links to org index

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,23 @@
+import { useOrganizationContext } from "./organizationContext";
 import { OrgDropdown } from "./header/orgDropdown";
 import { Landmark } from "lucide-react";
 import Link from "next/link";
 
 export const Header = () => {
+  const { organization } = useOrganizationContext() || null;
+
+  if (!organization) {
+    return null;
+  }
+
   return (
     <header className="supports-backdrop-blur:bg-background/60 sticky top-0 z-40 w-full border-b bg-background/95 shadow-sm backdrop-blur">
       <div className="container flex h-14 items-center">
         <div className="mr-4 hidden md:flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
+          <Link
+            href={`/org/${organization.slug}`}
+            className="mr-6 flex items-center space-x-2"
+          >
             <Landmark />
             <span className="hidden font-bold sm:inline-block">CapDash</span>
           </Link>
