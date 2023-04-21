c5d51e282b79998db8b3b03355a28e6cd77741e8
Harrison Grias <harrison.grias@gmail.com>
2023-04-21 11:50:27 -0500

    fix: fonts now coming from google fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,29 @@
 import { OrganizationProvider } from "~/components/organizationContext";
 import { SessionProvider } from "next-auth/react";
 import { type Session } from "next-auth";
+import { Inter } from "next/font/google";
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
+
+// Font from google fonts
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <OrganizationProvider>
-        <Component {...pageProps} />
-      </OrganizationProvider>
-    </SessionProvider>
+    <main lang="en" className={`${inter.variable} font-sans`}>
+      <SessionProvider session={session}>
+        <OrganizationProvider>
+          <Component {...pageProps} />
+        </OrganizationProvider>
+      </SessionProvider>
+    </main>
   );
 };
 
