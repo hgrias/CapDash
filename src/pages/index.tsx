4ca4ec2a7b827d3fa2852bee9930ef285a391f8c
Harrison Grias <harrison.grias@gmail.com>
2023-04-21 18:41:41 -0500

    refactor: add placeholder index page
    This will be used for general site info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useSession } from "next-auth/react";
-import { LegislatorGrid } from "~/components/legislatorGrid";
-import { Header } from "~/components/header";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const Home: NextPage = () => {
-  const { data: sessionData } = useSession();
-  const router = useRouter();
-
-  // Redirect to login page if not signed in
-  useEffect(() => {
-    if (!sessionData) {
-      void router.push("/login");
-    }
-  }, [router, sessionData]);
-
   return (
     <>
       <Head>
@@ -28,12 +13,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        {sessionData ? (
-          <>
-            <Header />
-            <LegislatorGrid />
-          </>
-        ) : null}
+        <p>This is CapDash! More info to come!</p>
       </main>
     </>
   );
