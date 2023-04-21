ed4c8ebd38218855fb03b1518dd91feb984cc7ea
Harrison Grias <harrison.grias@gmail.com>
2023-04-21 18:40:35 -0500

    refactor: link to correct legislator page

--- src/components/legislatorGrid.tsx
+++ src/components/legislatorGrid.tsx
@@ -1,15 +1,19 @@
 import Bio from "./legislator/bio";
 import { api } from "~/utils/api";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export const LegislatorGrid = () => {
+  const router = useRouter();
+  const orgSlug = router.query["org-slug"] as string;
+
   const { data: legislators } = api.legislator.getAll.useQuery(undefined, {
     refetchOnWindowFocus: false,
   });
   const legislatorCards = legislators?.map(
     (legislator: { id: string; firstName: string; lastName: string }) => (
       <div key={legislator.id}>
-        <Link href={`/legislators/profile/${legislator.id}`}>
+        <Link href={`/org/${orgSlug}/legislators/${legislator.id}`}>
           <Bio
             firstName={legislator.firstName}
             lastName={legislator.lastName}
