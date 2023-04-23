8fe09c054b4928a7de7b4d2bea0fa1ae71cdc4c2
Harrison Grias <harrison.grias@gmail.com>
2023-04-23 15:13:22 -0500

    feat: add dir for typesense helpers

---
+++ typesense/createCollections.ts
@@ -0,0 +1,34 @@
+import type { CollectionCreateSchema } from "typesense/lib/Typesense/Collections";
+import Typesense from "typesense";
+
+const client = new Typesense.Client({
+  nodes: [
+    {
+      host: "127.0.0.1", // For Typesense Cloud use xxx.a1.typesense.net
+      port: 8108, // For Typesense Cloud use 443
+      protocol: "http", // For Typesense Cloud use https
+    },
+  ],
+  apiKey: "xyz",
+  connectionTimeoutSeconds: 2,
+});
+
+const legislatorSchema: CollectionCreateSchema = {
+  name: "legislators",
+  fields: [
+    { name: "id", type: "string" },
+    { name: "firstName", type: "string" },
+    { name: "lastName", type: "string" },
+    { name: "district", type: "string" },
+    { name: "party", type: "string", facet: true },
+    { name: "role", type: "string" },
+  ],
+  // default_sorting_field: "lastName",
+};
+
+void client
+  .collections()
+  .create(legislatorSchema)
+  .then((data) => {
+    console.log(data);
+  });
---
+++ typesense/indexCollections.ts
@@ -0,0 +1,30 @@
+import Typesense from "typesense";
+import fs from "fs/promises";
+
+const client = new Typesense.Client({
+  nodes: [
+    {
+      host: "127.0.0.1", // For Typesense Cloud use xxx.a1.typesense.net
+      port: 8108, // For Typesense Cloud use 443
+      protocol: "http", // For Typesense Cloud use https
+    },
+  ],
+  apiKey: "xyz",
+  connectionTimeoutSeconds: 2,
+});
+
+async function importData() {
+  const legislatorsInFile = await fs.readFile(
+    "typesense/legislatorData.jsonl",
+    { encoding: "utf-8" }
+  );
+  await client
+    .collections("legislators")
+    .documents()
+    .import(legislatorsInFile, { action: "create" })
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+void importData();
