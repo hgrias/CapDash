2ecea354dafc4aede63cc0475b038f9a3ea33777
Harrison Grias <harrison.grias@gmail.com>
2023-05-03 17:32:36 -0500

    feat: add yarn script for resetting db and search

--- src/server/api/typesense/deleteCollections.ts
+++ src/server/api/typesense/deleteCollections.ts
@@ -17,22 +17,9 @@ const typesenseClient = new Typesense.Client({
   connectionTimeoutSeconds: 2,
 });
 
-const legislatorSchema: CollectionCreateSchema = {
-  name: "Legislator",
-  fields: [
-    { name: "id", type: "string" },
-    { name: "firstName", type: "string", sort: true },
-    { name: "lastName", type: "string", sort: true },
-    { name: "district", type: "string" },
-    { name: "party", type: "string", facet: true },
-    { name: "role", type: "string" },
-  ],
-  // default_sorting_field: "lastName",
-};
-
 void typesenseClient
-  .collections()
-  .create(legislatorSchema)
+  .collections("Legislator")
+  .delete()
   .then((data) => {
-    console.log("Typesense collections created.");
+    console.log("Typesense collections deleted.");
   });
