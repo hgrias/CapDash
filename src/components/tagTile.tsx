ff8068fe68bf21379a9cd55f50dcd42e0d22351f
Harrison Grias <harrison.grias@gmail.com>
2023-05-20 23:50:59 -0500

    style: change look of tagTile

--- src/components/tagTile.tsx
+++ src/components/tagTile.tsx
@@ -16,7 +16,7 @@ export const TagTile = ({ id, name }: TagTileProps) => {
 
   return (
     <Link href={`/org/${organization?.slug}/tags/${id}`}>
-      <div className="flex h-36 flex-col items-center justify-center gap-y-2 rounded-lg bg-slate-100 bg-gradient-to-r from-slate-200 to-slate-300 text-center shadow-xl">
+      <div className="flex h-36 flex-col items-center justify-center gap-y-2 rounded-lg border-2 border-slate-800 text-center shadow-xl">
         <Tag className="h-10 w-10" />
         <h1 className="text-2xl">{name}</h1>
       </div>
